9e4a1510d115e273657a3c27e04a6c43e96b6c4d
ksploit0x1 <ksploit0x1@gmail.com>
2025-07-30 18:07:06 +0100

    Add ob...c

---
+++ obfuckedCode.c
@@ -0,0 +1,19 @@
+#define  e int
+#define  ee main
+#define  eee (
+#define  eeee )
+#define  eeeee {
+#define  eeeeee }
+#define eeeeeee printf
+#define eeeeeeee return
+#define eeeeeeeee 0
+#define eeeeeeeeee ;
+#include <stdio.h>
+
+
+
+
+e ee eee eeee eeeee
+eeeeeee eee "obfucked code" eeee eeeeeeeeee
+eeeeeeee eeeeeeeee eeeeeeeeee
+eeeeee
---
+++ simple.py
@@ -0,0 +1,15 @@
+
+
+items = {
+    "Name":"khalid",
+    "Age":21,
+    "City":"MA"
+}
+
+for k,v in items.items():
+    print (k,"=>",v)
+
+arr = (1,3,5,6,7)
+
+for c,i in count(arr):
+    print(c)
